ec491f390e1b2955a68959a5d70fe7dddc68ac5a
Kat Sullivan <kmsullivan012@gmail.com>
2020-08-11 23:32:53 -0400

    added json file and able to read all csv files in data directory

--- parseData.py
+++ parseData.py
@@ -1,14 +1,28 @@
 import csv
 from pathlib import Path
+import json
+import glob
+import os
+
+# json_file = open("bio_data.json", "r")
+# json_object = json.load(json_file)
+# json_file.close()
+
 
 base_path = Path(__file__).parent
-file_path = (base_path / "../Art_A_Hack_Data_Bridge/data/08-05-2020-hussein-floormap_withFrame.csv").resolve()
+file_path = (base_path / "../Art_A_Hack_Data_Bridge/data").resolve()
+for filename in glob.glob(os.path.join(file_path, '*.csv')):
+	with open(os.path.join(os.curdir, filename), 'r') as csv_file: # open in readonly mode
+		# skip headers
+		next(csv_file)                  
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		for idx,frame in enumerate(csv_reader):
+			
+			
+	csv_file.close()
 
-with open(file_path) as csv_file:
-	# skip headers
-        next(csv_file)                  
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for idx,frame in enumerate(csv_reader):
-        	print(frame[0])
+# json_file = open("bio_data.json", "w")
+# json.dump(json_object, json_file)
+# json_file.close()
 
-csv_file.close()
+
