2779a26f6c943afa16c3e219b56fea053258953c
Kat Sullivan <kmsullivan012@gmail.com>
2020-08-12 10:49:14 -0400

    created min max values for each datum to help with normalizing

--- normalizeData.py
+++ normalizeData.py
@@ -12,14 +12,28 @@ normal_file.close()
 normal_object = {}
 
 #Normalized Data
-normal_object = json_object
 
-for key in normal_object:
-	print(list(normal_object[key].items())[0])
-
-
-#normalized = (x-min(x))/(max(x)-min(x))
-
+for key in json_object:
+	normal_object[key] = {}
+	# get weighted/averaged min and max
+	low_five = list(json_object[key].items())[:5]
+	high_five = list(json_object[key].items())[-5:]
+	sum_of_five = 0
+	total = 0
+	for k,v in low_five:
+		print(k,v)
+		sum_of_five += float(k) * v
+		total += v
+	key_min = sum_of_five / total
+	normal_object[key]["min"] = key_min
+	sum_of_five = 0
+	total = 0
+	for k,v in high_five:
+		print(k,v)
+		sum_of_five += float(k) * v
+		total += v
+	key_max = sum_of_five / total
+	normal_object[key]["max"] = key_max
 
 
 with open("normal_data.json", "w") as normal_file:
--- sendData.py
+++ sendData.py
@@ -2,9 +2,26 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
+import json
+
+avg_idx = 0
+avg_total = 0
+
+def normalize_data():
+	global avg_idx
+	global avg_total
+	avg_idx += 1
+	avg_total += 0
+	if avg_idx == 10:
+		value = avg_total / avg_idx
+		avg_idx = 0
+
 
 if __name__ == "__main__":
-	print("hi")
+	with open("normal_data.json", "r") as normal_file:
+		normal_object = json.load(normal_file)
+	normal_file.close()
+
 
 	client = udp_client.UDPClient('localhost', 9001)
 
@@ -12,3 +29,4 @@ if __name__ == "__main__":
 	# need dispatcher map here
 
 	server = osc_server.ThreadingOSCUDPServer(('localhost', 8000), dispatcher)
+	normalize_data()
