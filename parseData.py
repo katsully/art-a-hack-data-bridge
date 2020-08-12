be10d9e6d8b02d7e2d451b45ed29ae3c4d13c38b
Kat Sullivan <kmsullivan012@gmail.com>
2020-08-11 23:55:14 -0400

    get headers from data file

--- parseData.py
+++ parseData.py
@@ -8,13 +8,21 @@ json_file = open("bio_data.json", "r")
 json_object = json.load(json_file)
 json_file.close()
 
+first_file = False
+
 
 base_path = Path(__file__).parent
 file_path = (base_path / "../Art_A_Hack_Data_Bridge/data").resolve()
 for filename in glob.glob(os.path.join(file_path, '*.csv')):
 	with open(os.path.join(os.curdir, filename), 'r') as csv_file: # open in readonly mode
-		# skip headers
-		next(csv_file)                  
+		if not first_file:
+			headers = next(csv_file).split(",")
+			# remove newline from last element
+			headers[-1] = headers[-1].strip()
+			short_headers = [x.split("/EmotiBit/0/", 1)[1] if "/EmotiBit/0/" in x else x[1:] for x in headers]
+			first_file = True
+		else: # skip headers
+			next(csv_file)                  
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		for frame in csv_reader:
 			i = 1+1
