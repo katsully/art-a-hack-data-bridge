9f5bc7ae523da5d09d8f57c65e97ac6533cd7315
Kat Sullivan <kmsullivan012@gmail.com>
2020-08-12 09:24:32 -0400

    set up osc send and receive

--- sendData.py
+++ sendData.py
@@ -1,4 +1,14 @@
-
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 
 if __name__ == "__main__":
 	print("hi")
+
+	client = udp_client.UDPClient('localhost', 9001)
+
+	dispatcher = dispatcher.Dispatcher()
+	# need dispatcher map here
+
+	server = osc_server.ThreadingOSCUDPServer(('localhost', 8000), dispatcher)
