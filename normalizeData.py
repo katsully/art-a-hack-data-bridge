575db9104d70d9b066a42296487ce971743d147d
Kat Sullivan <kmsullivan012@gmail.com>
2020-08-16 21:47:28 -0400

    improving normalized data

--- normalizeData.py
+++ normalizeData.py
@@ -1,5 +1,5 @@
 import json
-
+from pandas import DataFrame
 
 with open("bio_data.json", "r") as json_file:
 	json_object = json.load(json_file)
@@ -18,12 +18,9 @@ for key in json_object:
 	# get weighted/averaged min and max
 	low_five = list(json_object[key].items())[:5]
 	high_five = list(json_object[key].items())[-5:]
-	print(key)
-	print(high_five)
 	sum_of_five = 0
 	total = 0
 	for k,v in low_five:
-		print(k,v)
 		sum_of_five += float(k) * v
 		total += v
 	key_min = sum_of_five / total
@@ -31,11 +28,20 @@ for key in json_object:
 	sum_of_five = 0
 	total = 0
 	for k,v in high_five:
-		print(k,v)
 		sum_of_five += float(k) * v
 		total += v
 	key_max = sum_of_five / total
 	normal_object[key]["max"] = key_max
+
+	# get quantiles
+	all_values = []
+	for k in json_object[key]:
+		for i in range(json_object[key][k]):
+			all_values.append(float(k))
+	df = DataFrame (all_values, columns=['nums_with_frequencies'])
+	df_quantiles = df.nums_with_frequencies.quantile([0.25,0.5,0.75])
+	# print(df_quantiles)
+	normal_object[key]["quantile"] = df_quantiles.values.tolist()
 
 
 with open("normal_data.json", "w") as normal_file:
